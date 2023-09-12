2edc0bd2a95c2166c703ab01fbd20e0c60f4d2a0
olegy692 <olegsmarcenuks@gmail.com>
2023-09-12 13:19:56 +0200

    halfdone

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Features from './features';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './sec1.css'
-import wood1 from './images/adad1.jpg'
+import './sec2.css'
+import malePic from './images/Male-25.png'
 
 import Wave from 'react-wavify'
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 
 
 
@@ -15,27 +16,40 @@ function Section2() {
       className="divbetween1"
       style={{
         width: '100%',
-        height: '400px',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         background: 'rgb(49, 140, 245)',
         background: 'linear-gradient(0deg, rgba(240, 244, 249, 1) 0%, rgba(52, 216, 252, 1) 100%)',
-        position: 'relative', // Add this style for positioning
+        position: 'relative',
+
       }}
     >
-      <div style={{ height: '450px', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '20px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxHeight: '98%' }}>
-          <h2 style={{ color: '#0b2444', marginBottom: '60px', paddingTop:'40px' }}>Equip Yourself with Real-Life Recycling Skills in Our Game</h2>
-          <Features></Features>
+      <div style={{ padding: '20px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <h2 style={{ color: '#0b2444', marginBottom: '20px', paddingTop: '20px' }}>
+            Equip Yourself with Real-Life Recycling Skills in Our Game
+          </h2>
+          <Features />
+
+
+          <div style={{position:'absolute', right:'100px', bottom:'30px', zIndex:'500'}}>
+            <ParallaxProvider>
+              <Parallax speed={10}>
+                <div>
+                  <img src={malePic}
+                    height={200}
+                    width={100}
+                  >
+                  </img>
+
+                </div>
+              </Parallax>
+            </ParallaxProvider>            
+          </div>
+          
         </div>
       </div>
-
-
-   
-    
-
-
     </div>
   );
 }
